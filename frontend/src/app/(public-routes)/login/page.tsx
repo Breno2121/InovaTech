6ab935b94f8c8de3b855cc314415f24129901c8a
MATEUS HENRIQUE DOS SANTOS <mateus_santos6@estudante.sesisenai.org.br>
2025-09-04 19:27:36 -0300

    04/09

--- frontend/src/app/(public-routes)/login/page.tsx
+++ frontend/src/app/(public-routes)/login/page.tsx
@@ -1,38 +1,8 @@
-'use client'
-
-import React from "react"
-import Head from "next/head"
 import styles from "./styles.module.css"
 
 
 export default function Home() {
   return (
-    <>
-    <Head>
-      <>Tela de Login</>
-    <link
-    href="https://unkpg.com/boxicons@2.1.4/css/boxicons.min.css"
-    rel="stylesheet"
-    />
-    </Head>
-
-    <div className="wrapper">
-      <form action="">
-        <h1>Login</h1>
-
-        <div className="input-box">
-          <input type="text"  placeholder="Username" required/>
-          <i className="bx bxs-lock-alt"></i>
-        </div>
-
-        <div className="input-box">
-          <input type="password" placeholder="Password" required/>
-          <i className="bx bxs-lock-alt"></i>
-        </div>
-
-        
-      </form>
-    </div>
-    </>
+    <h1>Login</h1>
   );
 }
