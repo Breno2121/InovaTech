ba470f7fb85da308c60f9693922300a761a8cb9d
MATEUS HENRIQUE DOS SANTOS <mateus_santos6@estudante.sesisenai.org.br>
2025-10-23 21:24:33 -0300

    23/10

--- frontend/src/app/(public-routes)/login/page.tsx
+++ frontend/src/app/(public-routes)/login/page.tsx
@@ -14,11 +14,11 @@ export default function LoginPage() {
   async function handleSubmit(e: React.FormEvent) {
     e.preventDefault();
     try {
-      const loginUrl = "http://localhost:3000/login";
+      const loginUrl = "http://localhost:3333/user/login";
 
       const response = await axios.post(loginUrl, {
         email: user,
-        password,
+        password: password,
       });
 
       localStorage.setItem("access_token", response.data.access_token);
@@ -30,36 +30,36 @@ export default function LoginPage() {
   }
   return (
     <div className={styles.wrapper}>
-        <form onSubmit={handleSubmit}>
-          {" "}
-          {}
-          <h1 className={styles.heading}>
-            InovaTech <TbPhoneRinging />
-          </h1>
-          <div className={styles.inputBox}>
-            <input
-              type="text"
-              placeholder="Username"
-              required
-              value={user}
-              onChange={(e) => setUser(e.target.value)}
-            />
-            <i className="bx bxs-user"></i>
-          </div>
-          <div className={styles.inputBox}>
-            <input
-              type="password"
-              placeholder="Password"
-              required
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <i className="bx bxs-lock-alt"></i>
-          </div>
-          <button type="submit" className={styles.btn}>
-            Login
-          </button>
-        </form>
-      </div>
+      <form onSubmit={handleSubmit}>
+        {" "}
+        { }
+        <h1 className={styles.heading}>
+          InovaTech <TbPhoneRinging />
+        </h1>
+        <div className={styles.inputBox}>
+          <input
+            type="text"
+            placeholder="Username"
+            required
+            value={user}
+            onChange={(e) => setUser(e.target.value)}
+          />
+          <i className="bx bxs-user"></i>
+        </div>
+        <div className={styles.inputBox}>
+          <input
+            type="password"
+            placeholder="Password"
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <i className="bx bxs-lock-alt"></i>
+        </div>
+        <button type="submit" className={styles.btn}>
+          Login
+        </button>
+      </form>
+    </div>
   );
 }
