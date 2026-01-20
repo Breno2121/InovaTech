2fa9704e11f3650493bd71aaf86d3b758e2fca29
Breno2121 <josebrenosilva0@gmail.com>
2026-01-20 12:24:19 -0300

    InovaTech_

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -10,7 +10,6 @@ import Image from 'next/image'
 
 export default function NavMenu() {
     const pathName = usePathname();
-    
     const itens = [
         {
             label: "Tela Principal",
